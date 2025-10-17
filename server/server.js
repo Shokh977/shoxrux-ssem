58b917df83d21eaf33cf884c2133067424378541
Shokhrukh <uphill7165@gmail.com>
2025-10-18 01:04:47 +0900

    url updates

--- server/server.js
+++ server/server.js
@@ -23,7 +23,7 @@ const app = express();
 
 // CORS configuration
 app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', 'https://client-dd2c.onrender.com');
+    res.header('Access-Control-Allow-Origin', ['http://localhost:5173', 'https://shoxrux-sssem.onrender.com'].includes(req.headers.origin) ? req.headers.origin : 'https://shoxrux-sssem.onrender.com');
     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,PATCH,OPTIONS');
     res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, X-Requested-With');
     res.header('Access-Control-Allow-Credentials', 'true');
